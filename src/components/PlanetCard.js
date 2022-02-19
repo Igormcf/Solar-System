46527efa9371f70cd455f20bfafc0fe9129ed247
Igormcf <igormaciel82@gmail.com>
2022-02-19 00:15:38 -0300

    Align planets

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -5,13 +5,17 @@ class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
-        <h3 data-testid="planet-name">{ planetName }</h3>
-        <img
-          src={ planetImage }
-          alt={ `Planeta ${planetName}` }
-          className={ planetName }
-        />
+      <div data-testid="planet-card" className="container-planets">
+        <div>
+          <h3 data-testid="planet-name">{ planetName }</h3>
+        </div>
+        <div className="container-img">
+          <img
+            src={ planetImage }
+            alt={ `Planeta ${planetName}` }
+            className={ planetName }
+          />
+        </div>
       </div>
     );
   }
