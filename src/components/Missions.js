1347b43beaf2e642f73466fceb93f2e6cf8c8b23
Igor <igormaciel82@gmail.com>
2022-09-07 15:50:36 -0300

    Implementa a lib scrollreveal

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,16 +1,35 @@
+/* eslint-disable react/no-string-refs */
 import React from 'react';
 import Title from './Title';
 import MissionCard from './MissionCard';
 import missions from '../data/missions';
+import sr from '../ScrollReveal';
 
 class Missions extends React.Component {
+  componentDidMount = () => {
+    const config = {
+      origin: 'bottom',
+      duration: 2500,
+      delay: 200,
+      distance: '900px',
+      scale: 1,
+      easing: 'ease',
+    };
+
+    sr.reveal(this.refs.box1, config);
+  }
+
   render() {
     return (
-      <div data-testid="missions" className="missions parallax">
+      <div
+        data-testid="missions"
+        className="missions parallax testimonial"
+        id="testimonials"
+      >
         <div className="container-subtitle">
           <Title headline="Missões" />
         </div>
-        <div className="conatiner-information">
+        <div className="conatiner-information row" ref="box1">
           { missions.map((item) => (<MissionCard
             key={ item.name }
             name={ item.name }
